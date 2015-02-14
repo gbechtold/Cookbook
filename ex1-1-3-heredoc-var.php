7a5a7284977c28b0ff399e2f481725af81451251
Guntram Bechtold <gb@starsmedia.com>
2015-02-14 12:02:09 +0100

    added heredoc var

---
+++ ex1-1-3-heredoc-var.php
@@ -0,0 +1,17 @@
+<?php
+
+$hamburgers = 'hot double peparami burger';
+/* using the "print <<< <args> <code>" command to print a lot of html and variables at the same time */
+
+print <<< HTML
+<html>
+<body>
+
+My favourite Chef makes the best $hamburgers.
+
+</body>
+</html> 
+HTML;
+
+
+?>

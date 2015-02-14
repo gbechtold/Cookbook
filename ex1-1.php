b8c519616d627108dc3a5fcceb4623ca96cde3d3
Guntram Bechtold <gb@starsmedia.com>
2015-02-14 11:05:43 +0100

    inital cookbook commit

---
+++ ex1-1.php
@@ -0,0 +1,12 @@
+<?php
+$neighbour = 'Hilda';
+print $neighbour[3];
+
+/* second example */
+$cost='$10.20';
+print "</br> the cost $cost"."</br>";
+print "<html><body> a funny way $cost for $10 \x32 </br> newline";
+print "PHP Vars get printed: $cost"."</br>";
+print 'No Vars get printed $cost'."</br>";
+print "Just figures \x32\x33";
+?>

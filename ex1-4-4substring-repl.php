9ac94a5a6d3a41d9ff375c7429621814ac23378c
Guntram Bechtold <gb@starsmedia.com>
2015-02-21 00:26:12 +0100

    added substring repl

--- ex1-4-4substring-repl.php
+++ ex1-4-4substring-repl.php
@@ -4,12 +4,25 @@
 
 if($_GET) {
 	$name = htmlspecialchars($_GET["name"]);
+	
+	$clearkeys = array("l","z","e","a","s","b","t","b","g");
+	$hxrkeys = array("1","2","3","4","5","6","7","8","9");
+	
+	
+	print str_replace($clearkeys,$hxrkeys,$name);
+	
+	$hxrchars=0;
+		for ( $i=0 , $j=strlen($name); $i < $j; $i++) {
+			if (strstr("abecde", $name[$i])) {
+				$hxrchars++;
+			
+			}
 		
-	print str_replace(array("l","z","e","a","s","b","t","b","g"),array("1","2","3","4","5","6","7","8","9"),$name);
+		} 
+	
 }
+echo "<br>".$hxrchars;
 
 if(!$_GET) {
-
-
 }
 

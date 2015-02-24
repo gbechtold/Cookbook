64d2a67355be61ef51a2e189662f9d488e31f225
Guntram Bechtold <gb@starsmedia.com>
2015-02-24 22:37:02 +0100

    added woopra tracking

--- file.js
+++ file.js
@@ -8,15 +8,23 @@ var campnet = getUrlVars()["Network"];
 var campad = getUrlVars()["ad"];
 var campsite = getUrlVars()["SiteTarget"];
 
+analytics.track('Campaign Data', {
+  CampaignID: campid,
+  CampaignKW: campkw,
+  CampaignNet: campnet,
+  CampaingAd: campad,
+  CampaingSite: campsite,
+	});
+
 if (typeof campid === "undefined") {
 // no variable "campaignid" is defined in the current scope *or* has been assigned the value undefined  
 } else {
 // Display value of set variable
-alert(campid);
-alert(campkw);
-alert(campnet);
-alert(campad);
-alert(campsite);
+// alert(campid);
+// alert(campkw);
+// alert(campnet);
+// alert(campad);
+// alert(campsite);
 // Write to the Document
 document.write(campid);
 document.write(campkw);

5fc60befe2327d46b0144e06880654725c8084b7
Guntram Bechtold <gb@starsmedia.com>
2015-02-25 21:02:56 +0100

    added numbers

---
+++ ex2-5-1numbers.php
@@ -0,0 +1,16 @@
+<?php
+
+foreach($numbers = range(1,10) as $number => $n) {
+	echo $n;
+	};
+echo "</br>";
+echo mt_rand(0,10);
+
+echo exp($n);
+echo pow($n, 3)."</br>";
+
+$m = pow($n, 3);
+print number_format($m, 2);
+
+$u = 4;
+print "Your search retruned $number "

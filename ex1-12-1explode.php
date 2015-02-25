e28181f90a02f4cb5c471f60ff8fb054f0d3b958
Guntram Bechtold <gb@starsmedia.com>
2015-02-25 20:10:43 +0100

    added wordwrap

--- ex1-12-1explode.php
+++ ex1-12-1explode.php
@@ -3,5 +3,7 @@
 $words = explode(' ','This is my basic sentence');
 
 foreach($words as $word => $a) {
-	echo $a."_";
+	echo $a."_";	
 }
+echo "</br></br>";
+print_r($words);
---
+++ ex1-13-1wordwrap.php
@@ -0,0 +1,5 @@
+<?php
+
+$sentence = "This is my nice and long sencence";
+
+echo wordwrap($sentence,8,"</br>");

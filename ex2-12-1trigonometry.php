c33065682f8834543af648df7e3a79c1f4789ce0
Guntram Bechtold <gb@starsmedia.com>
2015-02-27 21:59:55 +0100

    added trigonometry

--- ex2-12-1trigonometry.php
+++ ex2-12-1trigonometry.php
@@ -5,5 +5,17 @@ $angles = array(cos($numb), sin($numb), tan($numb));
 
 foreach($angles as $angle => $n){
 	print $n."</br>";
-}
+	
+
+
+};
+
+echo deg2rad(cos($numb))."</br>";
+echo rad2deg(cos($numb))."</br>";
+
+echo '</br>';	
+$hex = '1b';
+
+print $hex . '</br>';
+print hexdec($hex);
 	

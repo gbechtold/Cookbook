1f8e55f69b9825586d5cadbc3ebc21f13582f706
Guntram Bechtold <gb@starsmedia.com>
2015-02-27 21:37:37 +0100

    added geometry

---
+++ ex2-12-1trigonometry.php
@@ -0,0 +1,9 @@
+<?php
+
+$numb = 5;
+$angles = array(cos($numb), sin($numb), tan($numb));
+
+foreach($angles as $angle => $n){
+	print $n."</br>";
+}
+	
--- ex2-5-1numbers.php
+++ ex2-5-1numbers.php
@@ -11,3 +11,11 @@ echo pow($n, 3)."</br>";
 
 $m = pow($n, 3);
 print number_format($m, 1, '_', '-');
+
+$number = 4;
+print "Your search returned $number " . ($number == 1 ? "hit" : "hits") . ".";
+
+$numb = 2;
+print "Your team currently consists of ". $numb . " " . ($numb == 1 ? "person" : "people") . ".";
+
+print "Your team consist of ". $numb . " " . ($numb < 2 ? "person" : "people") . ".";

11b28ef9046ee287de19792fe0d23ca56100d418
Guntram Bechtold <gb@starsmedia.com>
2015-03-05 22:21:19 +0100

    added vars example

--- ex5-1-1vars.php
+++ ex5-1-1vars.php
@@ -1,5 +1,12 @@
 <?php
 
+$strikes = 0;
+$move = 'foul';
+pc_check_the_count('foul');
+
+
+
+
 function pc_check_the_count($pitch) {
 	static $strikes = 0;
 	static $balls = 0;
@@ -8,10 +15,35 @@ function pc_check_the_count($pitch) {
 	echo $pitch."</br>";
 	echo $strikes."</br>";
 	echo $balls."</br>";
+	
+	
+	switch ($pitch) {
+	case 'foul':
+		if (2 == $strikes) break; 
+		
+	case 'strike':
+		$strikes++;
+		break;
+		
+	case 'ball':
+		$balls++;
+		break;
+	}
+	
+	if(3 == $strikes) {
+		$strikes = $balles = 0;
+		return 'strike out';
+	}	
+		
+	if(4 == $balls) {
+		$strikes = $balles = 0;
+		return 'walk';
+		}
+		
+	return 'at bat';	
+	
 }
 
-$strikes = 0;
-$strikes = $strikes+10;
 
-pc_check_the_count(10);
 
+?>

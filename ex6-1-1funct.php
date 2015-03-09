20c33bbbd8b48f22cd9a3629414828ec2e3d31ea
Guntram Bechtold <gb@starsmedia.com>
2015-03-09 23:37:23 +0100

    added funct

--- ex6-1-1funct.php
+++ ex6-1-1funct.php
@@ -8,3 +8,21 @@ function total($a,$b) {
 }
 
 total(1,10);
+echo '<br>';
+
+function add_number($number) {
+
+	if(null !== $number) {
+	 	$number++;
+		}
+	echo $number;
+	}
+	
+echo '<br><br>';
+	
+$number = 1;
+echo '<br><br>';
+	
+echo $number.'</br>';
+add_number($number);
+echo $number;

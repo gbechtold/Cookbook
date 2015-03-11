5669544107e5886939ed9a3999ade636a7fd6c3c
Guntram Bechtold <gb@starsmedia.com>
2015-03-11 22:42:33 +0100

    added namedparams

--- ex6-1-4-namedparams.php
+++ ex6-1-4-namedparams.php
@@ -1,6 +1,14 @@
 <?php
 
 function image($img) {
+	$tag = '<img src="' . $img['src'] . '" ';
+	$tag .= 'alt="' . ($img['alt'] ? $img['alt'] : '') . '">';
+	return $tag;
 }
 
-echo '';
+
+$image = image(array('src' => 'cow.png', 'alt' => 'cow say moo'));
+echo $image;
+
+$image = image(array('src' => 'pig.jpg'));
+echo $image;

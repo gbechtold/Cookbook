ddd08ade21aa21b27693cfdc1b911a5c990bae65
Guntram Bechtold <gb@starsmedia.com>
2015-03-13 11:33:04 +0100

    added jqer

---
+++ ex1-11-5jqer.php
@@ -0,0 +1,28 @@
+
+<?php ?>
+<HTML>
+<head>
+</head>
+<body>
+
+<div>
+	<div id="panel1">Panel 1</div>
+	<div id="panel2">Panel 2</div>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>
+	$(document).ready(function () {
+	});
+</script>
+
+
+<script>
+	$(function() {
+		$("#panel1").hide(300);
+	});
+</script>
+
+
+</body>

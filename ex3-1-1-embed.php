c386be6a33573c6ef8be91ac571f26a39b227b80
Guntram Bechtold <gb@starsmedia.com>
2015-03-16 15:17:30 +0100

    added embeds

---
+++ ex3-1-1-embed.php
@@ -0,0 +1,9 @@
+<?php ?>
+
+<html>
+<body>
+
+<iframe width="200px" height="268px" frameborder="0" src="https://www.yumpu.com/is/embed/view/Ij1hGHaHZ5VJjXi5" allowfullscreen="true" allowtransparency="true"></iframe> <iframe width="200px" height="269px" src="https://www.yumpu.com/is/embed/view/Ij1hGHaHZ5VJjXi5" frameborder="0" allowfullscreen="true"  allowtransparency="true"></iframe> <iframe width="200px" height="303px" src="https://www.yumpu.com/is/embed/view/jusWGI6VmNiPtXlF" frameborder="0" allowfullscreen="true"  allowtransparency="true"></iframe> <iframe width="200px" height="303px" src="https://www.yumpu.com/is/embed/view/MpZZNInpgYtccbLN" frameborder="0" allowfullscreen="true"  allowtransparency="true"></iframe>	<iframe width="200px" height="269px" src="https://www.yumpu.com/is/embed/view/L7gUcIFlKTOE3e9S" frameborder="0" allowfullscreen="true"  allowtransparency="true"></iframe> 
+
+</body>
+</html>

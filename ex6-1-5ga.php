9f3e5d4dd739b4be2e574bc90ba1e9268ae973f1
Guntram Bechtold <gb@starsmedia.com>
2015-03-18 21:31:38 +0100

    add ga

---
+++ ex6-1-5ga.php
@@ -0,0 +1,28 @@
+<?php
+
+
+?>
+
+
+<head>
+</head>
+<body>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59582458-2', 'auto');
+  ga('send', 'pageview');
+
+// http://127.0.0.1/cookbook/ex6-1-5ga.php?utm_source=MYsource&utm_medium=MYmedium&utm_term=MYCampaign&utm_content=MYContent&utm_campaign=MYCampaignname
+
+
+</script>
+
+
+
+
+</body>

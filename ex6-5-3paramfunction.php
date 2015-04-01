ea964b42827e23f3dec31e900c0525d138831da5
Guntram Bechtold <gb@starsmedia.com>
2015-04-01 21:13:52 +0200

    added multiparams

---
+++ ex6-5-3paramfunction.php
@@ -0,0 +1,18 @@
+<?php
+
+$imagename = htmlspecialchars($_GET['imagename']);
+echo $imagename;
+
+function image($img) {
+	$tag = '<img src="' . $img['src'] . '" ';
+	$tag .= 'alt="' . ($img['alt'] ? $img['alt'] : '') .'">';
+	return $tag;
+}
+
+
+$image = image(array('src' => 'cow.png', 'alt' => 'cows say moo'));
+echo $image;
+
+$image = image(array('src' => 'pig.jpeg', 'alt' => 'pig say oink'));
+echo $image;
+?>

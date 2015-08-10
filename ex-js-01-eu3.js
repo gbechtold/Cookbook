8a477d86911cac911405a71eb256e38bc03ac90b
Guntram Bechtold <gb@starsmedia.com>
2015-08-10 21:57:16 +0200

    added eu3

---
+++ ex-js-01-eu3.js
@@ -0,0 +1,11 @@
+/* -*- tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
+ *      
+ * This Source Code Form is subject to the terms of the StarsMedia Public
+ * Repository */
+
+
+// an array to enumerate.
+var home = [0, 1, 2];
+
+print(home);
+	

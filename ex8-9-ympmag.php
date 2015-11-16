549c93e5970e515f6e50e36878329571c85db295
Guntram Bechtold <gb@starsmedia.com>
2015-11-16 20:35:40 +0100

    added examples

---
+++ ex8-9-ympmag.php
@@ -0,0 +1,335 @@
+<?php
+
+
+echo "home";
+
+
+$str = <<<EOD
+
+<script type="text/javascript" src="//www.yumpu.com/assets/v4/js/modules/toddycat/bin/YumpuMagazine.min.js"></script>
+
+<div id="myMagazineContainer" style="width:600px; height:450px;"></div>
+
+<script>
+   var myYumpu = new Yumpu();
+   myYumpu.create_player("#myMagazineContainer", "54090809", { canvasBGColor:"#e65117", fallback_order:"flash,html5,js" } );
+</script>
+
+
+EOD;
+
+
+echo $str;
+?>
+
+Aktuell:
+
+
+The Critical Path: 
+
+Beim ersten Briefing konnten 3 Themenstellungen als wichtig identifiziert werden: 
+
+Akustik und Fläche  
+Akustik korreliert mit Fläche. Wenn man in einem Raum die Akustische Qualität beeinflussen will ist die veränderbare Fläche eine kritische Größe. 
+Stark vereinfacht gesagt ist mehr Akustisch-aktive besser als weniger. 
+Zusätzlich steigt die akustische Qualität mit der Dicke akustisch aktiven Materials. 
+Deshalb ist es kritisch dass Getzner ein attraktiv anmutendes, nachrüstbares Modulsystem mit akustischer Wirksamkeit anbietet. 
+
+Segel für große Räume
+Akustiksegel sind in öffentlichen Räumen einfach installierbar. 
+Die Anforderungen für es an diese Objekte sind technisch und aus Nutzersicht abzustecken und zu definieren. 
+Akustische Wirksamkeit, funktionale Anwendung und passende Anmutung sind dabei entscheidend.
+
+Akustische Design Objekte  
+Design Objekte sind zur Visualisierung der akustischen Aktivität wichtig.
+Diese Designobjekt sensibilisieren auf angemessene weise für das Thema Raumakustik. Sie sind funktionale Objekte mit eigenem Character, eigenem formalen Character und akustischer Funktionalität    
+
+
+Grundlage zur Entscheidungsvalidierung
+
+Entsprechend dem "Lean Startup Appraoch" gliedert sich der Entscheidungsprozess gruppiert sich in eine Hypothese und einen Test der dazu durchgeführt werden muss. 
+Pro Hypothese ist jeweils die "kritischeste" Hypothese zu formulieren damit Blocker so früh wie möglich ausgeräumt werden können. 
+
+
+
+
+Raster-System: 
+- attraktive Anmutung
+- akustisch aktiv
+
+Raster definieren
+- Gut aussehendes Schaumpolster an der Wand
+- Mit Microfeinem Muster für Post-its
+
+Mechanische Anforderungen:
+Breite: 60cm, 90cm, 150cm
+Höhe pro Kachel: 26cm als Rastermaß, Unendlich
+
+
+Oberfläche:
+ -> Akustikwand
+ -> Pinwand
+ -> Metallfäden für Magnet 
+ -> Beamerwand
+
+Gesamtkonstrukt
+ -> Funktionsleisten im Unterbau für Elektro, Netzwerk, Licht
+ -> Lichtleisten in der Decke
+
+Detailideen:
+ -> Raster für A4 Zettel
+ -> Spezielle Nadeln für diese Anwendung
+
+ 
+Concept Validation List: 
+ 1. Wenn es möglich wäre einen Raum unsichtbar um 100 EUR/m2 Akustisch besser zu machen würden Archtitekten das tun
+ - Ziel: Sieht clean aus und kann gut montiert werden
+ - Invalidation: 
+ 	- Verändert den Raum Grundlegend
+ 	- Sieht aus wie aus der Gummizelle
+ 	- Kompliziert zu installieren
+ 
+ 
+- Heublumen.at
+ - Linien
+
+
+1.) One-Pager JS-Event:
+ - Zeit (10/20/30)
+ - Klick (PI Event)
+
+2.) "Gesprächs-Text" "Fokus EN Flippingbook"
+- Liste der Überschriften für LP Text (Alle)
+- Texte für LP (Auswahl)
+
+Inhalte der Mitbewerber
+- Vorteile / Nachteile: Cloud Vs Desktop
+
+
+Impediments: 
+
+
+- 
+
+Wir würden gerne ihre Anforderungen genauer verstehen
+Ich arbeite an einer Analyse der gemeldeten Punkt, damit Sie einen aktuellen Status haben
+Sie bekommen einen Maßnahmenplan zu diesen Themen
+
+
+Update zum Case Motorpresse: 
+1. Android app ist besonders heiss. Listing aller Probleme im TicketSystem
+2. Update der offenen Todos, jeweils mit Estimations
+3. 
+
+
+Demo-App: 
+
+Doppelte User durch 2 Importer: 
+
+Es gibt 32 User die durch die doppelten Daten der 2 Importer in diese Konflikte laufen. 
+Damit diese User Problemlos die Anwendung nutzen können muss dieses Problem schon beim erstellen des Exports behoben werden. 
+
+
+MOTORRAD Classic  #189091325514/
+jens.selbach@gmail.com 238000047049
+
+
+10.04.2015 um 10:31 schrieb Klein, Michaela
+
+E-Mail Adresse (Benutzername): robert.schlender@web.de
+Bestellnummer (Kennwort): 288600020166
+
+Ja, genau das ist der Grund. 
+Wir bekommen von Daniel hier noch ein 2. Datumsfeld das jeweils 30 Tage zurück gilt. 
+So bald das aktiv ist tritt dieser Fall nichtmehr auf. 
+
+
+Anforderung: Doppelte Passwortvergabe
+
+Am 11.03.2015 um 17:13 schrieb Lehn, Natalie <nlehn@motorpresse.de>:
+Seine Apple-ID: reinhard@new-Zealand.at
+Der Kunde hat ein Abo über iTunes bestellt, hat funktioniert mit Ausgabe 1/15, bei 02/15 nach unserem Update nicht mehr, bei 03/15 wieder. Da er die 02/15 lesen wollte, hat er die App gelöscht und nochmal neu installiert.
+
+Von: Michael Dudler [mailto:michael.dudler@me.com]
+Gesendet: Freitag, 27. Februar 2015 13:41
+abo@dpv.de<mailto:abo@dpv.de>
+...hab ich gemacht, ohne Erfolgt. Einzelausgaben kann ich herunterladen, wird aber jedesmal verrechnet (so stehts mindestens bei der Meldung) ?
+Angehängt Meldung.
+
+
+
+-----Ursprüngliche Nachricht-----
+Von: roland.siposs@bluewin.ch [mailto:roland.siposs@bluewin.ch] 
+Gesendet: Dienstag, 10. Februar 2015 21:59
+An: DPV Direct, MPS DIGITAL ABO
+Betreff: Re: Re: Re: Re: (Ihre Ticketnummer: DE01-13467501) Problem mit digitalem Flug Revue Abo
+ 
+Sehr geehrte Damen und Herren
+ 
+Danke für Ihre Nachricht: Sie hatten recht, das Abo fängt erst mit der nächsten Ausgabe ab Kauf an zu laufen, also 03 2015.
+ 
+Ich habe mir die vorherige Ausgabe 02 2015 einzeln gekauft, was problemlos funktioniert hat.
+ 
+Nun ist die erste Abo Ausgabe 03 2015 verfügbar und verlangt keine Bezahlung mehr. Doch leider funktioniert es nun doch nicht, wenn ich die Ausgabe lesen möchte. Es gibt zwei Probleme:
+ 
+1.) Wenn ich die Ausgabe 03 2015 öffnen möchte, erhalte ich die Fehlermeldung "Invalid IAP product ID". Es funktioniert nicht. Was ist hier falsch?
+2.) Ich habe versucht, das Problem zu lösen, indem ich die Flug Revue App deinstalliert und neu installiert habe. Es hat nichts geholfen. Doch leider kann ich nun auch die einzeln gekaufte Ausgabe 02 2015 nicht mehr lesen, obwohl ich auch die Funktion "Einkäufe wiederherstellen" ausprobiert habe. Bei der Ausgabe 02 2015 will die App nun erneut CHF 5.-- einkassieren obwohl schon bezahlt und nennt im Dialog aber die Ausgabe 01 2015, was falsch ist,  weil ich das Bild für 02 2015 angewählt habe.
+ 
+ 
+ 
+ 
+ Hallo Guntram,
+ 
+ist bei den Anmeldeproblemen (siehe Anhang) diesem Kunden eigentlich etwas heraus gekommen?
+ 
+Viele Grüße
+Natalie Lehn
+ 
+
+Motor Presse Stuttgart GmbH & Co. KG
+Sitz Stuttgart
+Handelsregister Amtsgericht Stuttgart
+HRA 9302	 	Komplementär-GmbH:
+Motor Presse Stuttgart Verwaltungsgesellschaft mbH
+Sitz Stuttgart
+Handelsregister Amtsgericht Stuttgart · HRB 5003
+Geschäftsführer: Dr. Volker Breid, Norbert Lehmann
+
+Von: Eppinger, Ralf [mailto:eppinger.ralf@dpv.de] 
+Gesendet: Dienstag, 17. Februar 2015 09:27
+An: Lehn, Natalie
+Betreff: WG: Ticketnummer: DE01-13255195 Flug Revue Abonnement
+ 
+Hallo Natalie,
+ 
+der iTunes-Kunde hatte sich schon mal gemeldet, kann sich aber leider immer noch anmelden (siehe Anhang).
+ 
+Jetzt möchte er das Abo über uns abschließen und die Kosten für das iTunes-Abo erstattet haben.
+ 
+ 
+Können wir das Problem mit seinem iTunes-Zugang lösen oder sollen wir Ihm entsprechend entgegen kommen?
+ 
+ 
+Viele Grüße
+ 
+Ralf Eppinger
+
+
+
+1. Invisible Konzept
+ - Technische Zeichnung für Panel zum aufkleben
+ - Mit Schienen und Funktionen
+ - Abtesten mit der Lackiererei die Farbigkeit
+
+2. Pixel konzept
+ - Plan zeichnen
+ 
+3. Tischdecke & Flipchart Aufsteller 
+
+
+
+
+
+ - Churn-Rate
+ 	- 300 zu 100
+ 	- 59 zu xy
+
+Hello Goran, 
+
+Can you help us to update and optimize the https://wordpress.org/plugins/yumpu-epaper-publishing/ plugin?
+
+There are 2 Tasks to do: 
+1. Optimize the Setup Process 
+2. Optimize the "Insert-PDF-Into-Post" Process
+
+There are 2 Steps for this: 
+1. Create a Design Concept an Design Screens 
+2. Implement the designs once approve.
+
+Please see the more specific Requirements for this Project here: 
+
+https://docs.google.com/document/d/12-0_RDFzcPQYzIkwOwiSjT6Bj3sAtPWLBZfIXy_LpB4/edit#
+
+Best regards
+Guntram
+
+MOTORRAD Classic  #189091325514/
+jens.selbach@gmail.com 238000047049
+MD5: a339f908756429d673f1a82ceccc1117
+
+
+
+Guntrams erster Affiliate Link: 
+http://yumpuagency.com/?ref=2
+
+
+----------
+Features
+Free Publishing
+Magazine Publishing
+Web Publishing
+Mobile Publishing
+Developer Publishing
+-----------
+Products:
+FREE
+ADFREE
+WEBKIOSK
+APPKIOSK
+PROKIOSK
+-----------
+PREISE
+
+
+
+Installation: 
+ - Button in den Shop
+ - Das Script "Landingpage" muss auf allen trackbaren seiten eingebaut werden. 
+   z.b. So http://yumpuagency.com/2015/11/03/seite-mit-dem-kaufen-button/?ref=2
+ - Die frage ist ob das ausschließlich mit JS Funktioniert.
+
+- Das Script "Conversion" bzw. die Seite muss per iframe geladen werden. 
+	
+
+Hoferreisen: 
+
+Hofer Ansprechpartner: 
+Fr. Listberger
++43 7244 8000 1518
+
+
+Kiosk: 
+
+Features nach Wichtigkeit:
+Upgrade-Script/Grunt-Composer Script
+Blätterpfeile bzw. deren Farbe per Setting einstellen (Kadir)
+HTML Block ohne Content für freie elemente (Kadir)
+Zum Start: Welcome-Video oder Tour (Rene)
+Bilder hochladen mit Mediabox (Kadir)
+Neues Theme: Bootstrap-CSS Blank-Style (Naus)
+Neues Theme: Globale Settings (Naus)
+Neues Theme: Schrift
+Neues Theme: CSS
+Neues Theme: Eine Möglichkeit um Code/JS zu hosten. Temporär wäre ein Github-gist als Link okay
+RSS Block (JS-Based) (Naus)
+ 
+ 
+
+http://aws.amazon.com/device-farm/
+
+
+sprucesf.com
+
+Prioritäten Ramona: 
+3. Statistik Newsletter
+4. App Designs
+
+
+Next Steps beim Bense
+
+gb.yumpuagency.com/htmlexample/index.html
+
+- Whiteboarding
+- Team-learning
+- timeboxing vs shipping, cs feature amount
+

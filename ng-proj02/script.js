dcb973e5fe2861faa784753e4c719e985aa356bf
Guntram Bechtold <gb@starsmedia.com>
2016-01-13 22:32:58 +0100

    adde proj 2

---
+++ ng-proj02/script.js
@@ -0,0 +1,16 @@
+var app = angular.module('computer',['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+	when('/main',{
+		templateUrl: 'main.html',
+		controller:'MainCtrl'
+	});
+}])
+
+
+.controller('MainCtrl',[function(){
+	console.log('This is the MainCtrl');
+
+}]);
+
